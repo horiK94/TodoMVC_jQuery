3113428dc7d7792d6f798870ba53180c4d44acf9
堀江 一樹 <s01722@CG2890.local>
2018-04-19 14:30:03 +0900

    allを押すとすべて反応するように修正, listが追加できるようにした

---
+++ todoUtils.js
@@ -0,0 +1,45 @@
+let todos = []
+
+$('#todo-input').change(() => {
+    const text = $('#todo-post [name=todo]').val()
+    addTodo(text)
+    showTodo()
+    $('#todo-post [name=todo]').val('')
+})
+
+function addTodo(text){
+    todos.push({'text': text, 'isDone': false})
+}
+
+function showTodo(){
+    $('.todo').remove()
+    for(let i = 0; i < todos.length; i++){
+        $('#todo-list').append(
+            '<div class="todo">' +
+            '   <input type="checkbox" name="todo" class="todo-done" value="'+i+'">' +
+            '   <span class="text">' + todos[i].text + '</span>' +
+            '   <button class="todo-delete" value="' + i + '">×</button>' +
+            '</div>'
+        )
+    }
+}
+
+function refreshTodoDone(){
+    const checkedIndex = $(".todo input[name=todo]:checked").map((i, elem) => {
+        return parseInt($(elem).val())
+    }).toArray()
+    for(let i = 0; i < todos.length; i++){
+        todos[i].isDone = jQuery.inArray(i, checkedIndex) !== -1
+    }
+}
+
+// チェックボックス関連
+
+$('#all').change(() => {
+    $('.todo-done[name=todo]').prop('checked', $('#all').prop('checked'))
+    refreshTodoDone()
+})
+
+$('.todo-done[name=todo]').change(() => {
+    console.log('foofoo')
+})
